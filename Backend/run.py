c192b22ce1083092abb90c7870c4ee41806c20a9
Marcos Mateo Tiburcio <106215917+KMBMarcos@users.noreply.github.com>
2024-02-17 14:36:54 -0400

    Actualizacion

--- Backend/run.py
+++ Backend/run.py
@@ -6,8 +6,12 @@ from app import create_app, db
 
 app = create_app()
 
+# Endpoint de prueba
+@app.route("/hello")
+def hello():
+    return  "Hello, World!"
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-
