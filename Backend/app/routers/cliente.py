d16c10ec8e551bec11063ac29057bdff06f1c14b
Marcos Mateo Tiburcio <106215917+KMBMarcos@users.noreply.github.com>
2024-02-17 19:10:17 -0400

    Actualizacion

--- Backend/app/routers/cliente.py
+++ Backend/app/routers/cliente.py
@@ -1,21 +1,42 @@
+from crypt import methods
 from flask_restx import Api, Resource
 from flask import Blueprint, jsonify, request
 from ..routers.auth import api
+from ..models.models import Cliente, db
 
 client = Blueprint("admin", __name__)
 
 client = api.namespace("admin", description="Rutas para el modelo cliente")
 
 
-# Rutas para obtener la informacion del cliente
+# Rutas GET del cliente
 @client.route("/client")
 class GetClient(Resource):
     def get_client():
-        pass
+        client_data = Cliente.query.all()
+        client_list = []
+        for data in client_data:
+            clients_data = {
+                'id': client_data.cli_int_user_id,
+                'email': client_data.email,
+                'firstname': client_data.cli_str_first_name,
+                'lastname': client_data.cli_str_last_name,
+                'phone': client_data.cli_str_phone,
+                'direction': client_data.cli_str_direction,
+                'profile_image': client_data.cli_str_profile_img,
+                'register_date': client_data.cli_str_register_date,
+                'suspension_date': client_data.cli_date_suspension_date,
+            }
+        client_list.append(clients_data)
+        return jsonify({'client_data': client_list})
 
 
-# Ruta para crear usuarios
+# Rutas POST del cliente
 @client.route("/cliente/post", methods=["POST"])
 class Client(Resource):
     def post_client():
         pass
+
+
+# Rutas PUT del cliente
+@client.rout("/cliente/int:<id>", methods=["PUT"])
