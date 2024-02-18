ddda51fab00c7e86548907674a749b18bf1c42be
orlandoc0107 <orlandocardenas0107@gmail.com>
2024-02-17 21:24:44 -0300

    revision 0.1

--- Backend/app/routers/cliente.py
+++ Backend/app/routers/cliente.py
@@ -4,39 +4,39 @@ from flask import Blueprint, jsonify, request
 from ..routers.auth import api
 from ..models.models import Cliente, db
 
-client = Blueprint("admin", __name__)
+clien = Blueprint("client", __name__)
 
-client = api.namespace("admin", description="Rutas para el modelo cliente")
+client = api.namespace("client", description="Rutas para el modelo cliente")
 
 
 # Rutas GET del cliente
-@client.route("/client")
+@client.route("")
 class GetClient(Resource):
-    def get_client():
+    def get(self):
         client_data = Cliente.query.all()
         client_list = []
+
         for data in client_data:
             clients_data = {
-                'id': client_data.cli_int_user_id,
-                'email': client_data.email,
-                'firstname': client_data.cli_str_first_name,
-                'lastname': client_data.cli_str_last_name,
-                'phone': client_data.cli_str_phone,
-                'direction': client_data.cli_str_direction,
-                'profile_image': client_data.cli_str_profile_img,
-                'register_date': client_data.cli_str_register_date,
-                'suspension_date': client_data.cli_date_suspension_date,
+                'id': data.cli_int_user_id,
+                'firstname': data.cli_str_first_name,
+                'lastname': data.cli_str_last_name,
+                'phone': data.cli_str_phone,
+                'direction': data.cli_str_direction,
+                'profile_image': data.cli_str_profile_img,
+                'register_date': data.cli_date_register_date,
+                'suspension_date': data.cli_date_suspension_date,
             }
-        client_list.append(clients_data)
-        return jsonify({'client_data': client_list})
+            client_list.append(clients_data)
+        return jsonify(client_list)
 
 
 # Rutas POST del cliente
-@client.route("/cliente/post", methods=["POST"])
+@client.route("/post")
 class Client(Resource):
-    def post_client():
+    def post(self):
         pass
 
 
 # Rutas PUT del cliente
-@client.rout("/cliente/int:<id>", methods=["PUT"])
+#@client.rout("/cliente/int:<id>", methods=["PUT"])
