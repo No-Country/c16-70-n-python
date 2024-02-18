4e29e70da9d73e4ec4bad04bf29d58cae75921c5
orlandoc0107 <orlandocardenas0107@gmail.com>
2024-02-17 21:51:10 -0300

    revision 0.2

--- Backend/app/routers/auth.py
+++ Backend/app/routers/auth.py
@@ -10,20 +10,22 @@ api = Api(auth, version="1.0", title="Agendify", description="Agendify API REST"
 autho = api.namespace("", description="Rutas para Autotificacion")
 
 
-@autho.route("/login")
+@autho.route("/register")
 class Users(Resource):
     def post(self):
         data = request.get_json()
         email = data.get("email")
         password = data.get("password")
+        role = data.get("role")
 
         try:
-            busqueda = Users.query.filter_by(use_str_email=email)
-            print(busqueda)
-            if busqueda:
-                descodificarPassword(
-                    password=password, passwordDB=busqueda.use_str_password
-                )
-
+            exitsEmail = Users.query.filter_by(use_str_email=email).first()
+            if not exitsEmail:
+                passwordH = codificarPassword(password)
+                new_user = Users(use_str_email=email, use_str_password=passwordH, use_str_type_profile=role)
+                db.session.add(new_user)
+                db.session.commit()
+            print(new_user)
+            return jsonify({"messager":"EL Usuario Fue Creado"})
         except:
-            pass
+            return jsonify({"messager":"Error al Conectarse con la BD"})
