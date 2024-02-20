4b592d7b1c9575575bd3aca13c9ea11d808c537e
orlandoc0107 <orlandocardenas0107@gmail.com>
2024-02-20 16:22:55 -0300

    update

--- Frontend/loanding/segurity.js
+++ Frontend/loanding/segurity.js
@@ -2,7 +2,7 @@ function obtenerYMostrarToken() {
   const token = sessionStorage.getItem("token");
 
   if (!token) {
-      window.location.href = "http://127.0.0.1:5500/frontend/";
+      window.location.href = "/Frontend/Home%20Page/Index.html";
       return;
   }
 
@@ -16,17 +16,19 @@ function obtenerYMostrarToken() {
   };
 
   fetch("http://127.0.0.1:5000/rol", requestOptions)
-      .then(response => response.text())
-      .then(result => {
-          console.log(result);
-          if (result === "cliente") {
-              window.location.href = "http://127.0.0.1:5500/frontend/ruta-para-clientes";
-          } else if (result === "proveedor") {
-              window.location.href = "http://127.0.0.1:5500/frontend/ruta-para-proveedores";
+      .then(response => response.json())
+      .then(data => {
+          console.log(data);
+          if (data.role === "clie") {
+              window.location.href = "/Frontend/ruta-para-clientes";
+          } else if (data.role === "prov") {
+              window.location.href = "/Frontend/ruta-para-proveedores";
+            } else if (data.role === "admin") {
+              window.location.href = "/Frontend/admin-panel";
           } else {
               console.log("Rol desconocido");
               
-              window.location.href = "http://127.0.0.1:5500/frontend/";
+              window.location.href = "/Frontend/Home%20Page/Index.html";
           }
       })
       .catch(error => console.log('error', error));
