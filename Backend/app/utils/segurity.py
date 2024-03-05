09c54122bfd122ab9ae716a5a6483c0b4cdbac37
fer-gab-sua <fer.gab.sua@gmail.com>
2024-03-05 18:54:33 -0300

    Update segurity.py

--- Backend/app/utils/segurity.py
+++ Backend/app/utils/segurity.py
@@ -5,8 +5,9 @@ from flask import jsonify, request
 import re
 from datetime import datetime,timedelta
 
-key = os.getenv('KEY')
-
+#key = os.getenv('KEY')
+#openssl rand -hex 32
+key = "acd69e5a9bff4f5fbf7f2cad8ee4cfa260d1a500ceeb972d58c0e1229ed91d40"
 # modulos para incryptar la password
 def codificarPassword(password):
     resp = generate_password_hash(password)
@@ -23,9 +24,13 @@ def descodificarPassword(password, passwordDB):
 def codificarToken(data):
     now = datetime.utcnow()
     expiration = now + timedelta(hours=10)
+    #
+    id= data.get('id')
+    role= data.get('role')
+    #
     playload = {
-        'id' : data.get('id'),
-        'role': data.get('role'),
+        'id' : id,
+        'role': role,
         'exp': expiration
     }
     return jwt.encode(playload, key, algorithm='HS256')
@@ -36,16 +41,16 @@ def verify_token():
         auth = request.headers.get('Authorization')
         if not auth:
             return jsonify({"message": "Token no proporcionado"})
-        
+
         datosToken = descodificarToken(auth)
         return datosToken
-    
+
 ###
 def descodificarToken(token):
     token_parts = token.split()
     if len(token_parts) != 2 or token_parts[0].lower() != 'bearer':
         return jsonify({'message': 'El Token es Invalido'})
-    
+
     token = token_parts[1]
     try:
         payload = jwt.decode(token, key, algorithms=['HS256'])
