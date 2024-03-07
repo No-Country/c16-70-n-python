24896766bc1f12938e3c5bc957440a9dd7a229e7
Marcos Mateo <marcosxmt@gmail.com>
2024-03-06 20:41:55 -0400

    Actualizacion

--- Backend/app/routers/user.py
+++ Backend/app/routers/user.py
@@ -559,7 +559,6 @@ class Servicios(Resource):
             per_page = request.args.get('per_page', 10, type=int)
 
             servicios = Servicios.query.paginate(page=page, per_page=per_page, error_out=False)
-            #
 
             lista_servicio = []
             for servicio in servicios.items:
@@ -577,100 +576,3 @@ class Servicios(Resource):
 ###################################################################################################
 ###################################################################################################
 ###################################################################################################
-
-
-###################################################################################################
-###################################################################################################
-###################################################################################################
-###################################################################################################
-#################End Point Viejos ##############
-# # Ruta para obtener información del cliente
-# @client.route("/get")
-# class GetDataClient(Resource):
-#     def get(self):
-#         # Solicitud de Token
-#         auth = request.headers.get('Authorization')
-#         if not auth:
-#             return jsonify({"message": "Token no proporcionado"})
-        
-#         datosToken = descodificarToken(auth)
-#         id = datosToken.get('id')
-        
-#         try:
-#             # Ejecuta la consulta para obtener el usuario
-#             user = Cliente.query.filter_by(cli_int_user_id=id).first()
-            
-#             # Traemos la data del cliente
-#             client_data = Cliente.query.all()
-#             client_list = []
-#             for data in client_data:
-#                 clients_data = {
-#                     'firstname': data.cli_str_first_name,
-#                     'lastname': data.cli_str_last_name,
-#                     'phone': data.cli_str_phone,
-#                     'direction': data.cli_str_direction,
-#                     'profile_image': data.cli_str_profile_img,
-#                     'register_date': data.cli_date_register_date,
-#                     'suspension_date': data.cli_date_suspension_date,
-#                 }
-#                 client_list.append(clients_data)
-#             return jsonify({'users': client_list})
-#         except Exception as db:
-#             print("Error:", db)
-#             return jsonify({"message": "Error al conectarse con la BD"})
-
-# # Rutas registrar información del cliente
-# @client.route("/post")
-# class PostClient(Resource):
-#     def post(self):
-#         pass
-
-
-# # Rutas de actualización de data del cliente
-# @client.route("/put")
-# class UpgradeDataClient(Resource):
-#     def put(self):
-#         pass
-
-# @client.route("/img")
-# class UpgrdadeImgClient(Resource):
-#     def put(self):
-#         #Solicitud de Token por el headers
-#         auth = request.headers.get('Authorization')
-#         img = request.files['img']
-
-#         if not auth:
-#             return jsonify({"message": "Token no proporcionado"})
-
-#         datosToken = descodificarToken(auth)
-#         id = datosToken.get('id')
-#         role = datosToken.get('role')
-
-#         try:
-#             # Verificar el rol del usuario
-#             if role == 'clie' or role == 'prov':
-#                 user = None
-#                 if role == 'clie':
-#                     user = Cliente.query.filter_by(cli_int_user_id=id).first()
-#                     if not user:
-#                         return jsonify({"message": "Cliente no encontrado"})
-#                     # Guardar la imagen para el cliente
-#                     user.save_cli_str_profile_img(img)
-#                     if not user:
-#                         return jsonify({"message": "Proveedor no encontrado"})
-#                     # Guardar la imagen para el proveedor
-#                     user.save_pro_str_profile_img(img)
-
-#                 return jsonify({"message": "Imagen de perfil actualizada correctamente"})
-#             else:
-#                 return jsonify({"message": "Rol de usuario desconocido"})
-
-#         except Exception as e:
-#             print("Error:", e)
-#             return jsonify({"message": "Error al conectarse con la BD"})
-
-
-# Obtener turnos
-# actualziar ,el campo de turno.
-# obtener categorias
-# ver Ubicacion 
