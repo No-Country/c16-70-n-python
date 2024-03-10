25ff6723f2e6eba4695a2555bf1dd4b02b88dd9d
orlandoc0107 <orlandocardenas0107@gmail.com>
2024-03-10 18:27:28 -0300

    Update

--- Frontend/loanding/segurity.js
+++ Frontend/loanding/segurity.js
@@ -3,7 +3,7 @@ async function obtenerYMostrarToken() {
   const token = sessionStorage.getItem("token");
 
   if (!token) {
-    window.location.href = "/login/login.html";
+    window.location.href = "/Frontend/login/login.html";
     return;
   }
 
@@ -16,17 +16,17 @@ async function obtenerYMostrarToken() {
     redirect: "follow",
   };
 
-  await fetch(apiUrlServer + "/auth/rol", requestOptions)
+  await fetch("https://c1670python.pythonanywhere.com/auth/rol", requestOptions)
     .then((response) => response.json())
     .then((data) => {
       if (data.role === "Paciente") {
-        window.location.href = "/cliente-panel/cliente-panel-principal.html";
+        window.location.href = "/Frontend/cliente-panel/cliente-panel-principal.html";
       } else if (data.role == "Admin") {
-        window.location.href = "/admin-panel/admin-panel-all-users.html";
+        window.location.href = "/Frontend/admin-panel/admin-panel-all-users.html";
       } else {
         console.log("Rol desconocido");
 
-        window.location.href = "/";
+        window.location.href = "../index.html";
       }
     })
     .catch((error) => console.log("error", error));
