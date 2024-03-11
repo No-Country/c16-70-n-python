20f6df33ed12e785770637704b53a7657b7c0134
orlandoc0107 <orlandocardenas0107@gmail.com>
2024-03-10 22:00:04 -0300

    Update

--- Frontend/scripts/createTurns.js
+++ Frontend/scripts/createTurns.js
@@ -1,31 +1,34 @@
 import { apiUrlServer } from "../js/config.js";
 
-
-
-document.getElementById("form-create-turno").addEventListener("submit", function(e) {
+document
+  .getElementById("form-create-turno")
+  .addEventListener("submit", function (e) {
     e.preventDefault();
     const datos = {
-        fecha: document.getElementById("fecha").value,
-        inicio: document.getElementById("inicio").value,
-        fin: document.getElementById("fin").value,
-        duracion: document.getElementById("duracion").value,
+      fecha: document.getElementById("fecha").value,
+      inicio: document.getElementById("inicio").value,
+      fin: document.getElementById("fin").value,
+      duracion: document.getElementById("duracion").value,
     };
-    console.log("aqui",datos);
+    //console.log("aqui", datos);
     const token = sessionStorage.getItem("token");
     const myHeaders = new Headers();
     myHeaders.append("Authorization", `Bearer ${token}`);
     myHeaders.append("Content-Type", "application/json");
     const datosEnJSON = JSON.stringify(datos);
-    console.log("aqui",datosEnJSON);
+    console.log("aqui", datosEnJSON);
     const myInit = {
-        method: "POST",
-        headers: myHeaders,
-        body: datosEnJSON,
-        mode: "cors",
-        cache: "default",
+      method: "POST",
+      headers: myHeaders,
+      body: datosEnJSON,
+      mode: "cors",
+      cache: "default",
     };
-    fetch(apiUrlServer + "/admin/turnos", myInit)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error));
-});
+    fetch(apiUrlServer + "admin/turnos", myInit)
+      .then((response) => response.json())
+      .then(data => {
+        console.log(data);
+        document.getElementById("message-container").textContent = data.message;
+      })
+      .catch((error) => console.log(error));
+  });
